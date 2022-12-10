db249f5a15dbf9e5a8f33848c19714765206b7f1
unknown <fejirojsiakpere@gmail.com>
2022-12-10 01:56:51 +0100

    testing

--- songcrud/musicapp/urls.py
+++ songcrud/musicapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-
+from . import views
 app_name = 'musicapp'
 
 urlpatterns = [
+    path('new', views.new_view)
+
 
 ]
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def new_view(request):
+    return render(request, 'musicapp/new.html')
